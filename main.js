20500955c90d93dc0393fcbe38bfda783bf81e78
Lucas de Paula <lucas.santosdepaula@gmail.com>
2019-06-15 16:23:44 -0300

    Primeira versão do afinador

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const { app, BrowserWindow } = require('electron')
-
+let win
 function createWindow () {
   // Create the browser window.
-  let win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
@@ -12,6 +12,28 @@ function createWindow () {
 
   // e carregar o index.html do aplicativo.
   win.loadFile('index.html')
+  //win.webContents.openDevTools()
+  win.on('closed', () => {
+    // Elimina a referência do objeto da janela, geralmente você iria armazenar as janelas
+    // em um array, se seu app suporta várias janelas, este é o momento
+    // quando você deve excluir o elemento correspondente.
+    win = null
+  })
 }
 
 app.on('ready', createWindow)
+app.on('window-all-closed', () => {
+    // No macOS é comum para aplicativos e sua barra de menu 
+    // permaneçam ativo até que o usuário explicitamente encerre com Cmd + Q
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+  })
+  
+  app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (win === null) {
+      createWindow()
+    }
+  })
